247f77fad6170291064ff876c4e11fe62266f8b7
Mason Swager <mlswager2@gmail.com>
2022-05-09 22:55:08 -0500

    added: api post,delete,put routes for usercategories, useractivities

--- server/api/useractivities.js
+++ server/api/useractivities.js
@@ -2,20 +2,42 @@ const router = require('express').Router()
 const { models: { UserActivities }} = require('../db')
 module.exports = router
 
-router.get("/", async (req,res,next) => {
-    try{
-        const test= await UserActivities.findAll();
-        res.json(test);
-    }
-    catch(error){
-        next(error)
-    }
-})
 //POST: add a new activity to useractivities for user
 router.post("/", async (req, res, next) => {
     try {
-        res.status(201).send(await UserActivities.create(req.body));
+        if(await UserActivities.findOne({
+            where:{
+                userId:req.body.userId,
+                activityId: req.body.activityId
+            }
+        })){
+            return res.status(403).send("this user/activity already exists")
+        }else{
+            res.status(201).send(await UserActivities.create(req.body));
+        }
     } catch (error) {
         next(error);
     }
 });
+
+//DELETE: delete a useractivity record
+router.delete("/:id", async (req,res,next) => {
+    try{
+        const userAct = await UserActivities.findByPk(req.params.id)
+        await userAct.destroy()
+        res.send(userAct)
+      } catch (error) {
+        next(error)
+      }
+})
+
+//PUT: update a useractivity record
+router.put("/:id", async (req,res,next) => {
+    try{
+        const userAct = await UserActivities.findByPk(req.params.id)
+        res.send(await userAct.update(req.body))
+      }
+      catch(error){
+        next(error)
+      }
+})
