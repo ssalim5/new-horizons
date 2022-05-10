58f7d08c7a0122a7efd269181b6ff89c0bae482c
Mason Swager <mlswager2@gmail.com>
2022-05-10 11:54:04 -0500

    create: script to build matrix for userActivities

---
+++ script/generateUserRatingsArr.js
@@ -0,0 +1,23 @@
+const { models: { UserActivities, User, Activity }} = require('../server/db')
+
+async function generateUserRatingsArr(){
+    //get the userActivties, a count of all users, and a count of all activities from db
+    const useAct = await UserActivities.findAll()
+    const userCount = await User.count()
+    const activityCount = await Activity.count()
+    
+    //build a matrix the length of userCount and the width of activityCount (fill with -1)
+    let arr = Array(userCount).fill(-1).map(()=>Array(activityCount).fill(-1))
+
+
+    //go through useAct and place scores in the corresponding spots in arr matrix
+    useAct.forEach((elem)=>{
+        arr[elem.userId-1][elem.activityId-1]=elem.score
+    })
+    
+    //console.log(arr)
+    return arr
+}
+
+// generateUserRatingsArr()
+console.log(generateUserRatingsArr())
