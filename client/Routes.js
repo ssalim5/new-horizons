86fc65cc4a595d98a7c88c4c458972bbf6682f9b
Sakib Salim <sakibsalim5@gmail.com>
2022-05-11 10:38:49 -0400

    typo

--- client/Routes.js
+++ client/Routes.js
@@ -4,7 +4,7 @@ import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import {me} from './store'
-import UserProfile from './components/UserProfile';
+import UserProfile from './components/Userprofile';
 import AllActivities from './components/AllActivities';
 import SingleActivity from './components/SingleActivity';
 import Registration from './components/Registration';
