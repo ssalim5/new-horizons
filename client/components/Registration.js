f8036ae9b521e4976f08a763b64ff9b7d4e665ba
Mason Swager <mlswager2@gmail.com>
2022-05-11 14:22:49 -0500

    added: registration categories

--- client/components/Registration.js
+++ client/components/Registration.js
@@ -1,135 +1,7 @@
 import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import { signup } from '../store/auth';
-import { createUser } from "../store/allUsersStore";
-import { connect } from "react-redux";
 import { Link} from "react-router-dom";
-
-
-// class Registration extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: "",
-//       password: "",
-//       email: "",
-//       // dateOfBirth: "",
-//     };
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.handleChange = this.handleChange.bind(this);
-//   }
-//   handleChange(evt) {
-//     this.setState({
-//       [evt.target.name]: evt.target.value,
-//     });
-//   }
-
-//   handleSubmit(evt) {
-//     evt.preventDefault();
-//     this.props.createUser({ ...this.state });
-//     this.setState({
-//       username: "",
-//       password: "",
-//       email: "",
-//       dateOfBirth: "",
-//     })
-//   }
-
-//   render() {
-//     const { username, password, email} = this.state;
-//     const { handleSubmit } = this;
-//     return (
-//         <div>
-//           <div>
-//             <div> Register User</div>
-//           </div>
-//           <form id="addUser-form" onSubmit={handleSubmit}>
-//             <div>
-//               <label htmlFor="username">Name:</label>
-//               <input  name="username" type="text" value={username} onChange={this.handleChange} />
-//             </div>
-//             <div >
-//               <label htmlFor="password">Password:</label>
-//               <input name="password" type="password"value={password} onChange={this.handleChange} />
-//             </div>
-//             <div >
-//               <label htmlFor="email">Email:</label>
-//                <input name="email" type="email" value={email} onChange={this.handleChange} />
-//              </div>
-//           {/* <form onSubmit={this.handleSubmit}>
-//           </form> */}
-//               <div >
-//                 <button type="submit">Submit</button>
-//                 <Link to="/">Cancel</Link>
-//               </div>
-//           </form>
-//         <label>
-//           Rate Your Interest in Creative Activities :
-//           <select value={this.state.value} onChange={this.handleChange}>
-//             <option value="0">Optional</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </label>
-//         <label>
-//           Rate Your Interest in Athletic Activities :
-//           <select value={this.state.value} onChange={this.handleChange}>
-//             <option value="0">Optional</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </label>
-//         <label>
-//           Rate Your Interest in Logical Activities :
-//           <select value={this.state.value} onChange={this.handleChange}>
-//             <option value="0">Optional</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </label>
-//         <label>
-//           Rate Your Interest in Relaxing Activities :
-//           <select value={this.state.value} onChange={this.handleChange}>
-//             <option value="0">Optional</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </label>
-//         <label>
-//           Rate Your Interest in Entertaining Activities :
-//           <select value={this.state.value} onChange={this.handleChange}>
-//             <option value="0"> Optional</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//         </label>
-//         </div>
-//     );
-//   }
-// }
-
-
-// const mapDispatchToProps = (dispatch, { history }) => ({
-//   //createUser: (user) => dispatch(createUser(user, history)),
-//   createUser: (user) => dispatch(authenticate(user,signup))
-// });
-
-// export default connect(null, mapDispatchToProps)(Registration);
 
 function Registration(){
   const dispatch = useDispatch()
@@ -139,11 +11,24 @@ function Registration(){
       password: "",
       confirmPassword: "",
   })
+  const [category,setCategory] = useState({
+    creative: 0,
+    athletic: 0,
+    logical: 0,
+    relaxing: 0,
+    entertaining: 0,
+  })
+
   const [hideRequiredFlag,setHideRequiredFlag] = useState(true)
   const [matchPasswords,setMatchPasswords] = useState(true)
 
+  function handleChange(evt) {
+    setCategory({...category,[evt.target.name]: evt.target.value});
+  }
+
   function handleSubmit(event) {
       event.preventDefault();
+      console.log(user)
       
       if(user.password===user.confirmPassword){
           let trigger = true
@@ -178,31 +63,88 @@ function Registration(){
           <div>
             <label htmlFor='email'>Email</label>
             <input name='email' type="email" value={user.email} onChange={(event)=>setUser({...user, email:event.target.value})}/>
-            {hideRequiredFlag ? "" : <span className = "alert">required field</span>}
+            {hideRequiredFlag ? "" : <div className = "alert">required field</div>}
           </div>
 
           <div>
             <label htmlFor='username'>Username</label>
             <input name='username' value={user.username} onChange={(event)=>setUser({...user, username:event.target.value})}/>
-            {hideRequiredFlag ? "" : <span className = "alert">required field</span>}
+            {hideRequiredFlag ? "" : <div className = "alert">required field</div>}
           </div>
 
           <div>
             <label htmlFor='password'>Password</label>
             <input name='password' type="password" value={user.password} onChange={(event)=>setUser({...user, password:event.target.value})}/>
-            {hideRequiredFlag ? "" : <span className = "alert">required field</span>}
+            {hideRequiredFlag ? "" : <div className = "alert">required field</div>}
           </div>
 
           <div>
             <label htmlFor='confirmPassword'>Confirm Password</label>
             <input name='confirmPassword' type="password" value={user.confirmPassword} onChange={(event)=>setUser({...user, confirmPassword:event.target.value})}/>
-            {hideRequiredFlag ? "" : <span className = "alert">required field</span>}
+            {hideRequiredFlag ? "" : <div className = "alert">required field</div>}
           </div>
 
           <div className="submit-buttons">
             <button type='submit'>submit</button>
             <button type='button'><Link to='/'>cancel</Link></button>
           </div>
+
+          <label>
+          Rate Your Interest in Creative Activities :
+          <select name="creative" value={category.creative} onChange={handleChange}>
+            <option value="0">Optional</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label>
+          Rate Your Interest in Athletic Activities :
+          <select name="athletic" value={category.athletic} onChange={handleChange}>
+            <option value="0">Optional</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label>
+          Rate Your Interest in Logical Activities :
+          <select name="logical" value={category.logical} onChange={handleChange}>
+            <option value="0">Optional</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label>
+          Rate Your Interest in Relaxing Activities :
+          <select name="relaxing" value={category.relaxing} onChange={handleChange}>
+            <option value="0">Optional</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label>
+          Rate Your Interest in Entertaining Activities :
+          <select name="entertaining" value={category.entertaining} onChange={handleChange}>
+            <option value="0"> Optional</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+          
         </form>
       </div>
   )
