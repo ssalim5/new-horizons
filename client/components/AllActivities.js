5b89d6fd87136e2e5e0535ddc408aa73e0fd6e46
Sakib Salim <sakibsalim5@gmail.com>
2022-05-18 15:24:14 -0400

    search activities bar

--- client/components/AllActivities.js
+++ client/components/AllActivities.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux"
 import { connect } from "react-redux";
 import { fetchActivities} from "../store/allActivitiesStore";
 import MappedActivity from "./utilities/MappedActivity";
 
 
-export class AllActivities extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    }
-  }
-  componentDidMount(){
-      this.props.fetchActivities()
-  }
-  render() {
-    return(
-      <div id="allActivities" className="module">
-        <h2>All Activities</h2>
-        <MappedActivity data={this.props.activities}/>
+const AllActivities = (props) => {
+  const dispatch = useDispatch()
+  const [input, setInput] = useState('')
+  const activities = useSelector((state) => state.activities )
+  useEffect( () => {
+    dispatch(fetchActivities(input))
+  }, [input])
+  return(
+    <div id="allActivities" className="module">
+      <h2>All Activities</h2>
+      <div>
+        <form className="search-activities">
+          <input
+            className="search-activities"
+            type="search"
+            value={input.name}
+            onChange={(e) => {
+              console.log(input)
+              setInput(e.target.value)
+            }}
+            placeholder="Type to search..."
+          />
+        </form>
       </div>
-    )
-  }
+      <MappedActivity data={activities}/>
+    </div>
+  )
 }
 
 
